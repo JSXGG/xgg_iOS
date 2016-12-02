736117879d3eb9b8ef19f9637b88367ea5aadbbc
翁献山 <batmanwengxianshan@gmail.com>
2016-12-03 00:18:46 +0800

    添加了一个testModel
    Signed-off-by: 翁献山 <batmanwengxianshan@gmail.com>

---
+++ XggPro/XggPro/AppDelegate/testModel.h
@@ -0,0 +1,14 @@
+//
+//  testModel.h
+//  XggPro
+//
+//  Created by  翁献山 on 2016/12/3.
+//  Copyright © 2016年 Baird-weng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testModel : NSObject
+@property(strong,nonatomic)NSString*AppDelegate;
+
+@end
---
+++ XggPro/XggPro/AppDelegate/testModel.m
@@ -0,0 +1,13 @@
+//
+//  testModel.m
+//  XggPro
+//
+//  Created by  翁献山 on 2016/12/3.
+//  Copyright © 2016年 Baird-weng. All rights reserved.
+//
+
+#import "testModel.h"
+
+@implementation testModel
+
+@end
